e1d2f24ac2f584f46abad1a1f97aafae3bce40a1
Frederic LOUI <frederic.loui@renater.fr>
2023-06-16 16:09:40 +0200

    work around for https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/2019908

--- install-deps.sh
+++ install-deps.sh
@@ -4,27 +4,21 @@ echo this script will install freeRtr dependencies on your computer.
 echo WARNING: this can alter your current network settings!
 echo prerequisites: minimal debian sid and root rights.
 
-installPackage()
-{
-echo ""
-echo ""
-echo ""
-echo ""
 echo --------------------------------------------- installing $1...
-apt-get -f -y install --no-install-recommends $1 
-}
 
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get -f -y dist-upgrade
 
+apt install -f -y ca-certificates-java
+
 PKG_LIST="apt-utils default-jre-headless socat ethtool iproute2 net-tools procps \
 libpcap-dev openssl libssl-dev libbpf-dev bpftool \
 zip unzip wget psmisc busybox \
 telnet tshark nmap iperf vim"
 
-apt-get -f -y install $PKG_LIST
+apt-get -f -y --no-install-recommends install $PKG_LIST
 
 for PKG in apparmor cloudinit; do
   echo ""
